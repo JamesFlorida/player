f726be603bd84907aad4db4792be51540faa75e8
JamesFlorida <james.x.hassett@gmail.com>
2026-09-01 18:28:32 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- src/app.js
+++ src/app.js
@@ -7,24 +7,8 @@ import { danceData } from './venues/Stockyard/danceData-stockyard.js';
 import { globalDanceList } from "./globalDanceList.js";
 import { venueDanceMap } from "./venues/Stockyard/venueDanceMap.js";
 import { venueConfig } from "./venues/Stockyard/venueConfig.js";
+import { venueEvents } from "./venues/Stockyard/venueEvents.js;
 
-export const venueEvents = [
-    {
-        id: "evt_mark_chesnutt",
-        title: "Mark Chesnutt — Live at The Stockyard",
-        date: "August 28",
-        image: "./images/mark_chesnutt.png",
-        price: "GA $25 advance / $30 day of show — VIP $85",
-        link: "https://stockyardlive.com"
-    },
-    {
-        id: "evt_diamond_rio",
-        title: "Diamond Rio — Live at The Stockyard",
-        date: "Friday, November 13",
-        image: "./images/diamond_rio.png",
-        price: "GA $25 advance / $30 day of show — VIP $95",
-        link: "https://stockyardlive.com"
-    }
 ];
 
 
