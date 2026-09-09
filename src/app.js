bb95ca1ac92b16b187a1ff9ffa4e10f119dc9fd5
JamesFlorida <james.x.hassett@gmail.com>
2026-09-09 11:15:55 -0400

    Comment out import globalDanceList

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ console.log("TOP OF APP.JS LOADED");
    IMPORTS      
 ============================================ */
 import { danceData } from './venues/Stockyard/danceData-stockyard.js';
-import { globalDanceList } from "./globalDanceList.js";
+// import { globalDanceList } from "./globalDanceList.js";
 import { venueDanceMap } from "./venues/Stockyard/venueDanceMap.js";
 import { venueConfig } from "./venues/Stockyard/venueConfig.js";
 import { venueEvents } from "./venues/Stockyard/venueEvents.js";
