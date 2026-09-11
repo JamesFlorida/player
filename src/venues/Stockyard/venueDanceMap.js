59a38a7f7ab2f8ece2319e6a3c935d758e7fbc44
JamesFlorida <james.x.hassett@gmail.com>
2026-09-11 11:33:57 -0400

    Update venueDanceMap.js

--- src/venues/Stockyard/venueDanceMap.js
+++ src/venues/Stockyard/venueDanceMap.js
@@ -1,4 +1,12 @@
 const venueDanceMap = [
+{ id: 119, playlist: "Stock-012", daytaught: "Tuesday" },
+{ id: 97, playlist: "Stock-010", daytaught: "Tuesday" },
+{ id: 116, playlist: "Stock-012", daytaught: "Tuesday" },
+{ id: 105, playlist: "Stock-011", daytaught: "Tuesday" },
+{ id: 107, playlist: "Stock-011", daytaught: "Tuesday" },
+{ id: 109, playlist: "Stock-011", daytaught: "Tuesday" },
+{ id: 56, playlist: "Stock-006", daytaught: "Tuesday" },
+{ id: 119, playlist: "Stock-012", daytaught: "Tuesday" },
 { id: 153, playlist: "Stock-016", daytaught: "Weekend" },
 { id: 152, playlist: "Stock-016", daytaught: "Weekend" },
 { id: 151, playlist: "Stock-016", daytaught: "Weekend" },
