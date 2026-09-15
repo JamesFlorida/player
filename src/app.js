1791e9d85abb4a1abbf077e2815ed9ca8bb3367f
JamesFlorida <james.x.hassett@gmail.com>
2026-09-15 13:33:57 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1265,7 +1265,8 @@ function renderCreateModeLayout() {
 
 // function below added 9.15.26
 function renderInfoPage() {
-    const viewport = document.getElementById("viewport");
+    const viewport = document.getElementById("masterApplicationViewport");
+
 
     viewport.innerHTML = `
         <div id="workspaceHeader">
